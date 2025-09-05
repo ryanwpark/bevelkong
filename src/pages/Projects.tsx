c818f5acaa5d4a929fc60d7c120d6055972c7d51
ryanwpark <ryanwjnpark@gmail.com>
2025-09-05 18:55:42 -0500

    fixed project alignment desktop view

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,37 +1,37 @@
-import ProjectCatalog from '../components/Projects/ProjectCatalog';
+import ProjectCatalog from "../components/Projects/ProjectCatalog";
 import {
-	ReactProjects,
-	PythonProjects,
-	JavaProjects,
-	SwiftProjects,
-} from '../assets/projectData';
-import FloatingLabel from '../components/Projects/FloatingLabel';
+  ReactProjects,
+  PythonProjects,
+  JavaProjects,
+  SwiftProjects,
+} from "../assets/projectData";
+import FloatingLabel from "../components/Projects/FloatingLabel";
 
 const Projects = () => {
-	return (
-		<div className='min-h-screen w-screen bg-[oklch(85.063%_0.08406_217.226)] flex flex-col'>
-			<div className='flex h-1/2 items-end md:space-x-40 justify-center'>
-				<span className=''>
-					<FloatingLabel label='React' />
-					<ProjectCatalog projects={ReactProjects} />
-				</span>
-				<span className=''>
-					<FloatingLabel label='Java' />
-					<ProjectCatalog projects={JavaProjects} />
-				</span>
-			</div>
-			<div className='flex h-1/2 items-start md:space-x-40 justify-center'>
-				<span className=''>
-					<FloatingLabel label='Python' />
-					<ProjectCatalog projects={PythonProjects} />
-				</span>
-				<span className=''>
-					<FloatingLabel label='Swift' />
-					<ProjectCatalog projects={SwiftProjects} />
-				</span>
-			</div>
-		</div>
-	);
+  return (
+    <div className="min-h-screen w-screen bg-[oklch(85.063%_0.08406_217.226)] flex flex-col">
+      <div className="flex h-1/2 items-end lg:relative lg:top-15 md:space-x-40 justify-center">
+        <span className="">
+          <FloatingLabel label="React" />
+          <ProjectCatalog projects={ReactProjects} />
+        </span>
+        <span className="">
+          <FloatingLabel label="Java" />
+          <ProjectCatalog projects={JavaProjects} />
+        </span>
+      </div>
+      <div className="flex h-1/2 items-start md:space-x-40 justify-center">
+        <span className="">
+          <FloatingLabel label="Python" />
+          <ProjectCatalog projects={PythonProjects} />
+        </span>
+        <span className="">
+          <FloatingLabel label="Swift" />
+          <ProjectCatalog projects={SwiftProjects} />
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default Projects;
